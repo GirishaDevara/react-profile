a036c117f3833fafe4b372436383646e3d724482
Girisha Devara <girisha.devara@gmail.com>
2020-04-29 21:56:09 +0530

    Updates

---
+++ precache-manifest.d80f1193b3cd7dbc7adcd6cf3ea4bea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2093011de0c9990957174798e8989dbb",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "649b3cd9cabef0c1add0",
+    "url": "/react-profile/static/css/main.f1e5ef53.chunk.css"
+  },
+  {
+    "revision": "0f5b94cfe4a2492a9984",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649b3cd9cabef0c1add0",
+    "url": "/react-profile/static/js/main.3be1ba14.chunk.js"
+  },
+  {
+    "revision": "9ed281195f38b6cb54f7",
+    "url": "/react-profile/static/js/runtime-main.94118734.js"
+  }
+]);
